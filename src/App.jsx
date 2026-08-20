7ffb59ffbc947b50d6a827f75824a8a5a42858fb
sepehr ardalan <sepehr.ardalan2003@gmail.com>
2026-08-20 10:21:32 +0330

    start

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './App.css'
+import { Route, Routes } from "react-router-dom"
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+import { configureStore } from "@reduxjs/toolkit";
+import Header from './pages/homePage/components/header'
+import Home from './pages/homePage/components/home'
+import Products from './pages/homePage/components/products'
+import Layout from './components/partial'
+import Sale from './pages/homePage/components/sale'
+import About from './pages/homePage/components/about'
+import Login from './pages/homePage/components/login'
+export default function App() {
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/sale" element={<Sale />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Layout>
+  )
+}
---
+++ src/components/partial/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+export default function Layout() {
+  return (
+    <header>
+      <nav>
+        <Link to="/">صفحه اصلی</Link>
+        <Link to="/products">محصولات</Link>
+        <Link to="/sale">فروش ویژه</Link>
+        <Link to="/about">درباره ما</Link>
+      </nav>
+      <div className="left">
+        <Link to="/login">ورود / ثبت نام</Link>
+      </div>
+    </header>
+  )
+}
